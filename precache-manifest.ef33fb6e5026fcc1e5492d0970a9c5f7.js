a0fb69c3b0bd9d830583e0e80a95fb7dfda08a3e
xubailong <bailongctui@gmail.com>
2019-09-05 17:49:13 +0800

    bulid

---
+++ precache-manifest.ef33fb6e5026fcc1e5492d0970a9c5f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac57052aa309c6b877e156a3b4cd7670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "805e7173ba389b62d39d",
+    "url": "/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "ed74a81706df1c122804",
+    "url": "/static/css/main.e61fd132.chunk.css"
+  },
+  {
+    "revision": "805e7173ba389b62d39d",
+    "url": "/static/js/2.0cc8e6b3.chunk.js"
+  },
+  {
+    "revision": "ed74a81706df1c122804",
+    "url": "/static/js/main.2852a321.chunk.js"
+  },
+  {
+    "revision": "87517c3768baa5b92e2a",
+    "url": "/static/js/runtime~main.85756fe4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
